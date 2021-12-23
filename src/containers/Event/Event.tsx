4cc3ac95fede20cc5122f3a24f57c198b797def8
Kostushka <nastyatery47@yandex.ru>
2021-12-23 14:03:58 +0300

    Added file structure

---
+++ src/containers/Event/Event.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './Event.module.css';
+
+const Event: FC = () => {
+    return <></>;
+};
+
+export default Event;
---
+++ src/containers/Login/Login.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './Login.module.css';
+
+const Login: FC = () => {
+    return <></>;
+};
+
+export default Login;
