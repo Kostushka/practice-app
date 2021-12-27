9fdac24a9e263d0261f135068e6cf5cd464624d3
Kostushka <nastyatery47@yandex.ru>
2021-12-27 20:44:09 +0300

    Added LoginForm

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,41 @@
+import { Button, Card, Form, Input } from 'antd';
+import { rules } from '../../utils/rules';
+import styles from './LoginForm.module.css';
+
+const LoginForm = () => {
+    const onFinish = () => {
+        console.log('Success');
+    };
+    return (
+        <Form
+            labelCol={{ span: 12 }}
+            wrapperCol={{ span: 16 }}
+            onFinish={onFinish}
+        >
+            <Card>
+                <Form.Item
+                    label='Имя пользователя'
+                    name='Имя пользователя'
+                    rules={[rules.required('Пожалуйста, введите имя!')]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label='Пароль'
+                    name='Пароль'
+                    rules={[rules.required('Пожалуйста, введите пароль!')]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type='primary' htmlType='submit'>
+                        Вход
+                    </Button>
+                </Form.Item>
+            </Card>
+        </Form>
+    );
+};
+
+export default LoginForm;
--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,8 +1,16 @@
 import { FC } from 'react';
+import { Layout, Row } from 'antd';
 import styles from './Login.module.css';
+import LoginForm from '../../components/LoginForm';
 
 const Login: FC = () => {
-    return <>Login</>;
+    return (
+        <Layout>
+            <Row justify='center' align='middle' className={styles.row}>
+                <LoginForm />
+            </Row>
+        </Layout>
+    );
 };
 
 export default Login;
