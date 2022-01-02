982f026a0295097d70b1599f818956187d073792
Kostushka <nastyatery47@yandex.ru>
2022-01-02 18:47:38 +0300

    Taking data from localStorage on page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import { FC } from 'react';
-import {Layout} from 'antd';
+import { FC, useEffect } from 'react';
+import { Layout } from 'antd';
+import { useDispatch } from 'react-redux';
+
+import { setAuthAction, setUserAction } from './store/reducers/authReducer';
+import { IUser } from './models/IUser';
 
 import AppRouter from './components/AppRouter';
 import Navigation from './components/Navigation';
@@ -7,6 +11,18 @@ import Navigation from './components/Navigation';
 import './App.css';
 
 const App: FC = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            dispatch(
+                setUserAction({
+                    username: localStorage.getItem('username' || ''),
+                } as IUser)
+            );
+            dispatch(setAuthAction(true));
+        }
+    }, []);
+
     return (
         <Layout>
             <Navigation />
