3e1514fb8049c939c7712d677fb9840200935b52
Kostushka <nastyatery47@yandex.ru>
2022-01-04 19:42:59 +0300

    Added Modal

--- src/containers/Event/Event.tsx
+++ src/containers/Event/Event.tsx
@@ -1,12 +1,27 @@
-import { Layout } from 'antd';
-import { FC } from 'react';
+import { Button, Layout, Modal, Row } from 'antd';
+import { FC, useState } from 'react';
 import EventCalendar from '../../components/EventCalendar';
 import styles from './Event.module.css';
 
 const Event: FC = () => {
+    const [modalVisible, setModalVisible] = useState(false);
     return (
-        <Layout>
+        <Layout className={styles.modal}>
             <EventCalendar events={[]} />
+            <Row justify='center'>
+                <Button
+                    className={styles.btn}
+                    onClick={() => setModalVisible(true)}
+                >
+                    Добавить событие
+                </Button>
+            </Row>
+            <Modal
+                title='Добавить событие'
+                visible={modalVisible}
+                footer={null}
+                onCancel={() => setModalVisible(false)}
+            />
         </Layout>
     );
 };
